9b69c9ab6ee4a1536cd776cc5b1d6c1255c9ea6f
Wulgryn <urbanzolika2004@gmail.com>
2023-03-20 00:07:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Github_Release_Manger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
